d9b64992921528cb7c26df40dfcaf89db9320f8a
AntalThomas <Blake-Heward@hotmail.com>
2022-09-30 20:55:44 +0930

    Required methods

---
+++ linkedList.rb
@@ -0,0 +1,53 @@
+class LinkedList
+    attr_reader :head, :tail
+
+    def initialize
+        self.head = nil
+        self.tail = nil
+    end
+
+    def append(value)
+
+    end
+
+    def prepend(value)
+
+    end
+
+    def size
+
+    end
+
+    def head
+
+    end
+
+    def tail
+
+    end
+
+    def at(index)
+
+    end
+
+    def pop
+        
+    end
+
+    def contains?(value)
+
+    end
+
+    def find(value)
+
+    end
+
+    def to_s
+
+    end
+
+    private
+    class Node
+
+    end
+end
